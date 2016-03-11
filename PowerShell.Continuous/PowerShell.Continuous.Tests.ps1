6ff019d8a0ab9bfd29f77e2f2c7d100058ed755f
Bartłomiej Dubas <dubcio@o2.pl>
2016-03-11 02:57:17 +0100

    Use `In` in tests
    Form manage the working directory for invoke Invoke-Continous command.

--- PowerShell.Continuous/PowerShell.Continuous.Tests.ps1
+++ PowerShell.Continuous/PowerShell.Continuous.Tests.ps1
@@ -8,24 +8,17 @@ Describe 'PowerShell.Continuous Module' -Tags "Scope-White" {
 		New-Item 'TestDrive:\Workspace' -ItemType directory
 		New-Item 'TestDrive:\Workspace\Continuous.Integration' -ItemType directory
 
-		BeforeEach {
-			Push-Location
-		}
-
-		AfterEach {
-			Pop-Location
-		}
-		
 		It 'Should invoke action script with proper environmnet' {
-			Set-Location 'TestDrive:\Workspace'
-			Invoke-Continuous Workspace:\Continuous.Integration {
-				Get-PSDrive Workspace | % { $_.Root } | Should Be (Get-Item 'TestDrive:\Workspace').FullName
-				Get-PSDrive Action | % { $_.Root } | Should Be (Get-Item 'TestDrive:\Workspace\Continuous.Integration').FullName
-				Get-PSDrive Artifacts | % { $_.Root } | Should Be (Get-Item 'TestDrive:\Workspace\.Artifacts').FullName
+			In 'TestDrive:\Workspace' {
+				Invoke-Continuous Workspace:\Continuous.Integration {
+					Get-PSDrive Workspace | % { $_.Root } | Should Be (Get-Item 'TestDrive:\Workspace').FullName
+					Get-PSDrive Action | % { $_.Root } | Should Be (Get-Item 'TestDrive:\Workspace\Continuous.Integration').FullName
+					Get-PSDrive Artifacts | % { $_.Root } | Should Be (Get-Item 'TestDrive:\Workspace\.Artifacts').FullName
 
-				($env:PSModulePath -split ';') | Select-Object -First 1 | Should Be (Get-Item 'TestDrive:\Workspace\').FullName
+					($env:PSModulePath -split ';') | Select-Object -First 1 | Should Be (Get-Item 'TestDrive:\Workspace\').FullName
 
-				'Executed' | Add-Content 'Artifacts:\Result.txt'
+					'Executed' | Add-Content 'Artifacts:\Result.txt'
+				}
 			}
 
 			'TestDrive:\Workspace\.Artifacts\Result.txt' | Should Contain 'Executed'
@@ -34,8 +27,9 @@ Describe 'PowerShell.Continuous Module' -Tags "Scope-White" {
 		It 'Should clean up environmnet after action script was invoked' {
 			$orginal = $env:PSModulePath
 
-			Set-Location 'TestDrive:\Workspace'
-			Invoke-Continuous Workspace:\Continuous.Integration {}
+			In 'TestDrive:\Workspace' {
+				Invoke-Continuous Workspace:\Continuous.Integration {}
+			}
 
 			$env:PSModulePath | Should Be $orginal
 			Get-PSDrive Workspace | % { $_.Root } | Should Not Be (Get-Item 'TestDrive:\Workspace').FullName
@@ -46,14 +40,15 @@ Describe 'PowerShell.Continuous Module' -Tags "Scope-White" {
 		It 'Should propagate error from action script' {
 			$orginal = $env:PSModulePath
 
-			Set-Location 'TestDrive:\Workspace'
 			$invoke = {
 				Invoke-Continuous Workspace:\Continuous.Integration {
 					throw 'error from action script'
 				}
 			}
 
-			$invoke | Should Throw 'error from action script'
+			In 'TestDrive:\Workspace' {
+				$invoke | Should Throw 'error from action script'
+			}
 			$env:PSModulePath | Should Be $orginal
 			Get-PSDrive Workspace | % { $_.Root } | Should Not Be (Get-Item 'TestDrive:\Workspace').FullName
 			Get-PSDrive Action | % { $_.Root } | Should Not Be (Get-Item 'TestDrive:\Workspace\Continuous.Integration').FullName
